f4d00c56030266388cd058e88e19eec436dc94ba
Linus Juhlin <juhlinus@gmail.com>
2020-08-25 22:24:28 +0200

    First pass at only returning error messages on relevant input fields #1

--- src/Concerns/ValidatesInertiaInput.php
+++ src/Concerns/ValidatesInertiaInput.php
@@ -3,6 +3,8 @@
 namespace Kakunin\Concerns;
 
 use Kakunin\Exceptions\ValidatedException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
 
 /**
  * Provides instant form validation behaviour
@@ -24,5 +26,31 @@ trait ValidatesInertiaInput
         if ($this->request->get($validateKey, false)) {
             throw new ValidatedException();
         }
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $validatedKeys = array_keys(
+            array_filter(
+                $validator->getData()
+            )
+        );
+        $validatorMessages = $validator->getMessageBag()->getMessages();
+
+        $messages = array_filter($validatorMessages, function ($messageKey) use ($validatedKeys) {
+            return in_array($messageKey, $validatedKeys);
+        }, ARRAY_FILTER_USE_KEY);
+
+        $exception = new ValidationException($validator);
+
+        throw $exception->withMessages($messages);
     }
 }
